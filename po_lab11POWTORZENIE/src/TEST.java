7d29d7cc0cb8ae3ab12f308cfb610424d77d786f
IgorKawczynski <kluseczkibabuni94@gmail.com>
2022-01-28 14:05:29 +0100

    Updating...

--- po_lab11POWTORZENIE/src/TEST.java
+++
@@ -1,9 +0,0 @@
-public class TEST {
-
-    public static void main(String[] args)
-    {
-
-
-    }
-
-}
--- po_lab7POWTORZENIE/src/zad3/Dzwiek.java
+++
@@ -1,7 +0,0 @@
-package zad3;
-
-public interface Dzwiek {
-
-    public String dzwiek();
-
-}
