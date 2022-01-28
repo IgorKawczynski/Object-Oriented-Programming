02cac322c8575a63f494c2127808f49120fa9cb5
IgorKawczynski <kluseczkibabuni94@gmail.com>
2022-01-28 14:06:12 +0100

    Updating...

--- kolokwium_nr_2/src/pl/edu/uwm/obiektowe/s162423/kolo2/IListable.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.uwm.obiektowe.s162423.kolo2;
-
-import java.util.Stack;
-
-public interface IListable {
-
-    public Stack<Product> getSortedByValue(boolean ascending);  // Stack - Stock ??
-}
